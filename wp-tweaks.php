7f2c9f528ab442536dddea55c7c8ba9cec26d3cf
Fabien Schurter <fabien.schurter@gmail.com>
2014-12-12 16:17:20 +0100

    Add another view-related helper

--- wp-tweaks.php
+++ wp-tweaks.php
@@ -160,6 +160,26 @@ function _fswpt_get_asset($relative_url, $escape = true)
 }
 
 /**
+ * Get the absolute URL of an image attachment, according to format.
+ *
+ * @param integer $attachment_id
+ * @param string  $format
+ * @param boolean $escape
+ *
+ * @return string
+ */
+function _fswpt_get_image_src($attachment_id, $format = 'thumbnail', $escape = true)
+{
+    $img_src = wp_get_attachment_image_src($attachment_id, $format);
+    $src     = (isset($img_src[0]) ? $img_src[0] : '');
+    if ($escape) {
+        $src = esc_url($src);
+    }
+
+    return $src;
+}
+
+/**
  * Cleanly terminate an AJAX call in case of error.
  *
  * @param string $error_msg
