b397d0d394e92897f69116f46e28a18cc2238f48
Fabien Schurter <fabien.schurter@gmail.com>
2014-12-12 15:56:08 +0100

    Add some taxonomy-related helpers

--- wp-tweaks.php
+++ wp-tweaks.php
@@ -33,6 +33,71 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 
+/**
+ * Get the passed taxonomy term's current (1-based) level in taxonomy hierarchy.
+ *
+ * @param object $term_object
+ *
+ * @return integer
+ */
+function _fswpt_get_taxonomy_term_level($term_object)
+{
+    // Sanity check
+    if (!isset($term_object->term_id) || !isset($term_object->taxonomy)) {
+        throw new InvalidArgumentException('Invalid term object.');
+    }
+
+    return count(get_ancestors(intval($term_object->term_id), $term_object->taxonomy)) + 1;
+}
+
+/**
+ * Get the passed taxonomy term's highest ancestor in taxonomy hierarchy.
+ *
+ * @param object $term_object
+ *
+ * @return WP_Term|boolean A WP_Term object on success, false on failure.
+ */
+function _fswpt_get_taxonomy_term_ancestor($term_object)
+{
+    // Sanity check
+    if (!isset($term_object->term_id) || !isset($term_object->taxonomy)) {
+        throw new InvalidArgumentException('Invalid term object.');
+    }
+
+    $term_ancestors        = get_ancestors(intval($term_object->term_id), $term_object->taxonomy);
+    $term_highest_ancestor = get_term(intval(end($term_ancestors)), $term_object->taxonomy);
+
+    if (!$term_highest_ancestor || is_wp_error($term_highest_ancestor)) {
+        return false;
+    } else {
+        return $term_highest_ancestor;
+    }
+}
+
+/**
+ * Get the first taxonomy term for a post.
+ *
+ * @param integer $post_id
+ * @param string  $taxonomy
+ *
+ * @return WP_Term|false A WP_Term object on success, false on failure.
+ */
+function _fswpt_get_post_main_taxonomy_term($post_id, $taxonomy = 'category')
+{
+    // Sanity check
+    if (!taxonomy_exists($taxonomy)) {
+        return false;
+    }
+
+    $post_terms = wp_get_post_terms($post_id, $taxonomy);
+
+    if (!$post_terms || is_wp_error($post_terms)) {
+        return false;
+    } else {
+        return $post_terms[0];
+    }
+}
+
 // Clean HTML <head> up a bit
 remove_action('wp_head', 'adjacent_posts_rel_link_wp_head');
 
