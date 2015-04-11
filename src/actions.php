1a722f081532e39b25cc0458ccc2839adef148d0
Fabien Schurter <fabien.schurter@gmail.com>
2015-04-11 16:05:39 +0200

    Add license header in source files

--- src/actions.php
+++ src/actions.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of the fabschurt/wp-tweaks package.
+ *
+ * (c) 2014-2015 Fabien Schurter <dev@fabschurt.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Fabien Schurter <dev@fabschurt.net>
+ * @license   MIT
+ * @copyright 2014-2015 Fabien Schurter
+ */
+
 // Enforce locale to match current Polylang language
 if (is_plugin_active('polylang/polylang.php')) {
     setlocale(LC_ALL, pll_current_language('locale'));
--- src/filters.php
+++ src/filters.php
@@ -1,5 +1,18 @@
 <?php
 
+/**
+ * This file is part of the fabschurt/wp-tweaks package.
+ *
+ * (c) 2014-2015 Fabien Schurter <dev@fabschurt.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Fabien Schurter <dev@fabschurt.net>
+ * @license   MIT
+ * @copyright 2014-2015 Fabien Schurter
+ */
+
 // Remove the «Private» label from the beginning of private posts' titles
 add_filter('private_title_format', function($title) {
     return '%s';
