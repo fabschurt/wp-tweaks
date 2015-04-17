fe8111b665f5cf60c239dc6485725eb600a35446
Fabien Schurter <fabien.schurter@gmail.com>
2015-04-17 15:58:18 +0200

    Adapt PHPUnit workflow to new architecture

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -1,13 +1,13 @@
 <?php
 
-require_once __DIR__.'/../vendor/autoload.php';
+require_once __DIR__.'/../../vendor/autoload.php';
 
 $testsDir = '/tmp/wordpress-tests-lib';
 
 require_once "{$testsDir}/includes/functions.php";
 
 tests_add_filter('plugins_loaded', function() {
-    require_once __DIR__.'/../wp-tweaks.php';
+    require_once __DIR__.'/../../wp-tweaks.php';
 });
 tests_add_filter('muplugins_loaded', function() {
     // Require main mu-plugin files here
