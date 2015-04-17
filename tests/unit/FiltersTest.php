d8a480bcc5ac27220660a575e02e38b29d452b8b
Fabien Schurter <fabien.schurter@gmail.com>
2015-04-17 16:35:05 +0200

    Add skeleton classes for unit tests

---
+++ tests/unit/FiltersTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * This file is part of the fabschurt/wp-tweaks package.
+ *
+ * (c) 2014-2015 Fabien Schurter <dev@fabschurt.net>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @author    Fabien Schurter <dev@fabschurt.net>
+ * @license   MIT
+ * @copyright 2014-2015 Fabien Schurter
+ */
+
+namespace FabSchurt\WpTweaks\Tests\Unit;
+
+use FabSchurt\WpUnit\WpTestCase;
+
+class FiltersTest extends WpTestCase
+{
+}
